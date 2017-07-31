949ad075c31f7fbe2f99a8c39e8c7c8d144ccfd0
Miles <milesdpratt@gmail.com>
2017-07-31 14:02:37 -0400

    Update game to randomly generate boards and bomb placement.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,51 @@
-const printBoard = (board) => {
-	console.log('Current board: ');
-	console.log(board[0].join(' | '));
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
+const printBoard = board => {
+	console.log(board.map(row => row.join(' | ')).join('\n'));
 }
-let board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
-printBoard(board);
-board[0][1] = 1;
-board[2][2] = 'B';
-printBoard(board);
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	let board = [];
+	for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+		let row = [];
+		for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+	return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+		let row = [];
+		for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced < numberOfBombs) {
+		//this while loop will place all bombs. Currently, bombs can be overwritten.
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		if (board[randomRowIndex][randomColumnIndex] == null) {
+			board[randomRowIndex][randomColumnIndex] = 'B';
+			numberOfBombsPlaced++;
+		}
+	}
+	for (let row = 0; row < numberOfRows; row++) {
+		for (let i = 0; i < numberOfColumns; i++) {
+			if (board[row][i] == null) {
+				board[row][i] = ' ';
+			}
+		}
+	}
+	return board;
+}
+
+let playerBoard = generatePlayerBoard(10, 10);
+let bombBoard = generateBombBoard(10, 10, 30);
+console.log('Player board:');
+printBoard(playerBoard);
+console.log('Bomb board:');
+printBoard(bombBoard);
